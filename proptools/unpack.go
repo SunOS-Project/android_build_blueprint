f4aff6d195186a9115186c86a5d454deabcbe1bd
Kenn Frankel <kfrankel@google.com>
2024-01-11 04:22:45 +0000

    Revert "Merge ZP1A.240108.001"
    Revert submission 1678338-ZP1A.240108.001_2024-01-10-12.26.02
    
    Reason for revert: Video performance is broken - youtube videos crash and Android Screen Bridge is non-functional
    
    Reverted changes: /q/submissionid:1678338-ZP1A.240108.001_2024-01-10-12.26.02
    
    Change-Id: Ic14ecbe4402b1cc6764e5c9598ae3b588cc0e99c

--- proptools/unpack.go
+++ proptools/unpack.go
@@ -108,7 +108,6 @@ func UnpackProperties(properties []*parser.Property, objects ...interface{}) (ma
 
 func (ctx *unpackContext) reportUnusedNames(unusedNames []string) []error {
 	sort.Strings(unusedNames)
-	unusedNames = removeUnnecessaryUnusedNames(unusedNames)
 	var lastReported string
 	for _, name := range unusedNames {
 		// if 'foo' has been reported, ignore 'foo\..*' and 'foo\[.*'
@@ -124,23 +123,6 @@ func (ctx *unpackContext) reportUnusedNames(unusedNames []string) []error {
 		lastReported = name
 	}
 	return ctx.errs
-}
-
-// When property a.b.c is not used, (also there is no a.* or a.b.* used)
-// "a", "a.b" and "a.b.c" are all in unusedNames.
-// removeUnnecessaryUnusedNames only keeps the last "a.b.c" as the real unused
-// name.
-func removeUnnecessaryUnusedNames(names []string) []string {
-	if len(names) == 0 {
-		return names
-	}
-	var simplifiedNames []string
-	for index, name := range names {
-		if index == len(names)-1 || !strings.HasPrefix(names[index+1], name) {
-			simplifiedNames = append(simplifiedNames, name)
-		}
-	}
-	return simplifiedNames
 }
 
 func (ctx *unpackContext) buildPropertyMap(prefix string, properties []*parser.Property) bool {
